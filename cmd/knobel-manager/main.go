989a3da7f8236ae5afc28e8fc9813201dcfe1a9d
Hendrik Brinkmann <brinkmann@sipgate.de>
2024-10-06 14:19:04 +0200

    refactor: use recommended go project structure

--- cmd/knobel-manager/main.go
+++ cmd/knobel-manager/main.go
@@ -1,7 +1,9 @@
 package main
 
+import "github.com/henok321/knobel-manager-service/pkg/app"
+
 func main() {
-	app := &App{}
+	app := &app.App{}
 	app.Initialize()
 	app.Router.Run("0.0.0.0:8080")
 }
