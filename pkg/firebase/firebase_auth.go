3ec243f313f39dd49f87c1256fcba55e5bc36519
Hendrik Brinkmann <brinkmann@sipgate.de>
2024-10-06 18:48:07 +0200

    setup firebase config env

--- pkg/firebase/firebase_auth.go
+++ pkg/firebase/firebase_auth.go
@@ -3,13 +3,18 @@ package firebaseauth
 import (
 	"context"
 	fbadmin "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
 	"log"
+	"os"
 )
 
 var App *fbadmin.App
 
 func InitFirebase() {
-	app, err := fbadmin.NewApp(context.Background(), nil)
+
+	firebaseSecret := []byte(os.Getenv("FIREBASE_SECRET"))
+	opt := option.WithCredentialsJSON(firebaseSecret)
+	app, err := fbadmin.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatalf("error initializing Firebase app: %v", err)
 	}
