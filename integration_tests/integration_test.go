e77bd06a4b28916a8053d3a51e954bb03bb5369b
Hendrik Brinkmann <brinkmann@sipgate.de>
2024-10-16 10:03:25 +0200

    test: add integration tests

--- integration_tests/integration_test.go
+++ integration_tests/integration_test.go
@@ -6,6 +6,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/henok321/knobel-manager-service/pkg/game"
+
 	"github.com/henok321/knobel-manager-service/internal/app"
 	"github.com/stretchr/testify/assert"
 )
@@ -55,4 +57,35 @@ func TestAppInitialization(t *testing.T) {
 		assert.JSONEq(t, `{"games":[]}`, bodyString)
 	})
 
+	t.Run("get games", func(t *testing.T) {
+		instance, server := setup()
+		defer teardown(server)
+
+		var owners1 = []game.Owner{
+			{Sub: "mock-sub"},
+		}
+
+		var owners2 = []game.Owner{
+			{Sub: "unknown-sub"},
+		}
+
+		instance.DB.Create(&game.Game{
+			Name: "test game", Owners: owners1,
+		})
+		instance.DB.Create(&game.Game{
+			Name: "test game 2", Owners: owners2,
+		})
+
+		resp, err := http.Get(server.URL + "/games")
+		if err != nil {
+			t.Fatalf("Failed to perform GET request: %v", err)
+		}
+		defer resp.Body.Close()
+
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		bodyString := string(bodyBytes)
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode, "Expected status code 200")
+		assert.JSONEq(t, `{"games":[{"id":1,"name":"test game","owners":[{"id":1,"sub":"mock-sub"}]}]}`, bodyString)
+	})
 }
