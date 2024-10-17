47e089a12b5bda4ea45687e698a76b216b933da0
Hendrik Brinkmann <brinkmann@sipgate.de>
2024-10-17 11:58:17 +0200

    refactor: postgres testcontainer

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,11 +1,8 @@
 package db
 
 import (
-	"log"
 	"os"
 
-	"github.com/henok321/knobel-manager-service/pkg/game"
-	"github.com/henok321/knobel-manager-service/pkg/player"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -15,12 +12,6 @@ func Connect() (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
 	if err != nil {
 		return nil, err
-	}
-
-	err = db.AutoMigrate(&player.Player{}, &game.Game{}, &game.Owner{})
-
-	if err != nil {
-		log.Fatalf("error migrating database: %v", err)
 	}
 	return db, nil
 }
