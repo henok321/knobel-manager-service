aa96a802d4d60cea4e40a54d317157769d4d221a
Hendrik Brinkmann <brinkmann@sipgate.de>
2024-10-17 13:25:10 +0200

    refactor: postgres testcontainer

--- integration_tests/integration_test.go
+++ integration_tests/integration_test.go
@@ -13,21 +13,17 @@ import (
 	"testing"
 	"time"
 
+	"github.com/henok321/knobel-manager-service/internal/app"
+	"github.com/henok321/knobel-manager-service/pkg/game"
+	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
-
-	"github.com/henok321/knobel-manager-service/pkg/game"
-
-	"github.com/henok321/knobel-manager-service/internal/app"
-	"github.com/stretchr/testify/assert"
 )
 
 func setup() (*app.App, *httptest.Server) {
-
 	var testInstance app.App
 	testInstance.InitializeTest()
-
 	server := httptest.NewServer(testInstance.Router)
 	return &testInstance, server
 }
@@ -38,9 +34,12 @@ func teardown(server *httptest.Server) {
 
 func startTestDatabase() (*sql.DB, func(), error) {
 	ctx := context.Background()
-
-	pgContainer, err := postgres.Run(ctx, "docker.io/postgres:16-alpine", postgres.WithInitScripts(filepath.Join("..", "testdata", "init-db.sql")), // Path to your init script
-		postgres.WithDatabase("knobel-manager-service"), postgres.WithUsername("test"), postgres.WithPassword("secret"), testcontainers.WithWaitStrategy(wait.ForLog("database system is ready to accept connections").
+	pgContainer, err := postgres.Run(ctx, "docker.io/postgres:16-alpine",
+		postgres.WithInitScripts(filepath.Join("..", "testdata", "init-db.sql")),
+		postgres.WithDatabase("knobel-manager-service"),
+		postgres.WithUsername("test"),
+		postgres.WithPassword("secret"),
+		testcontainers.WithWaitStrategy(wait.ForLog("database system is ready to accept connections").
 			WithOccurrence(2).WithStartupTimeout(5*time.Second)))
 	if err != nil {
 		return nil, func() {}, fmt.Errorf("failed to start container: %w", err)
@@ -67,12 +66,10 @@ func startTestDatabase() (*sql.DB, func(), error) {
 		db.Close()
 	}
 
-	// Return the database and cleanup function
 	return db, cleanup, nil
 }
 
 func TestAppInitialization(t *testing.T) {
-
 	t.Run("health check", func(t *testing.T) {
 		_, cleanup, _ := startTestDatabase()
 		defer cleanup()
@@ -116,20 +113,11 @@ func TestAppInitialization(t *testing.T) {
 		instance, server := setup()
 		defer teardown(server)
 
-		owners1 := []game.Owner{
-			{Sub: "mock-sub"},
-		}
+		owners1 := []game.Owner{{Sub: "mock-sub"}}
+		owners2 := []game.Owner{{Sub: "unknown-sub"}}
 
-		owners2 := []game.Owner{
-			{Sub: "unknown-sub"},
-		}
-
-		instance.DB.Create(&game.Game{
-			Name: "test game", Owners: owners1,
-		})
-		instance.DB.Create(&game.Game{
-			Name: "test game 2", Owners: owners2,
-		})
+		instance.DB.Create(&game.Game{Name: "test game", Owners: owners1})
+		instance.DB.Create(&game.Game{Name: "test game 2", Owners: owners2})
 
 		resp, err := http.Get(server.URL + "/games")
 		if err != nil {
