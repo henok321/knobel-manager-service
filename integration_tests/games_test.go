9272b797c5470ae515f5dd65ce2fa67d6f082213
Hendrik Brinkmann <henok321@gmail.com>
2024-10-17 23:27:46 +0200

    test: use plain sql

--- integration_tests/games_test.go
+++ integration_tests/games_test.go
@@ -2,13 +2,12 @@ package integration_tests
 
 import (
 	"bytes"
+	"database/sql"
 	"io"
 	"net/http"
 	"testing"
 
-	"github.com/henok321/knobel-manager-service/pkg/game"
 	"github.com/stretchr/testify/assert"
-	"gorm.io/gorm"
 )
 
 func TestGames(t *testing.T) {
@@ -17,7 +16,7 @@ func TestGames(t *testing.T) {
 		method             string
 		endpoint           string
 		body               string
-		setup              func(db *gorm.DB)
+		setup              func(db *sql.DB)
 		expectedStatusCode int
 		expectedBody       string
 		headers            map[string]string
@@ -29,65 +28,114 @@ func TestGames(t *testing.T) {
 			name:     "GET games empty",
 			method:   "GET",
 			endpoint: "/games",
-			setup: func(db *gorm.DB) {
+			setup: func(db *sql.DB) {
 				// No setup needed; database is empty
 			},
 			expectedStatusCode: http.StatusOK,
 			expectedBody:       `{"games":[]}`,
 			headers:            map[string]string{"Authorization": "permitted"},
-		},
-		// GET /games with data
+		}, // GET /games with data
 		{
 			name:     "GET games by data",
 			method:   "GET",
 			endpoint: "/games",
-			setup: func(db *gorm.DB) {
-				owners1 := []game.Owner{{Sub: "mock-sub"}}
-				owners2 := []game.Owner{{Sub: "unknown-sub"}}
+			setup: func(db *sql.DB) {
+				tx, err := db.Begin()
+				if err != nil {
+					t.Fatalf("Failed to begin transaction: %v", err)
+				}
+				_, err = tx.Exec(`INSERT INTO owners (id,sub) VALUES (1, 'mock-sub'), (2, 'mock-sub-2')`)
+				if err != nil {
+					t.Fatalf("Failed to insert owners: %v", err)
+				}
+				_, err = tx.Exec(`INSERT INTO games (id, name) VALUES (1, 'test game'), (2, 'test game 2')`)
+				if err != nil {
+					t.Fatalf("Failed to insert games: %v", err)
+				}
+				_, err = tx.Exec(`INSERT INTO game_owners (game_id, owner_id) VALUES (1, 1), (2, 2)`)
+				if err != nil {
+					t.Fatalf("Failed to insert game_owners: %v", err)
+				}
 
-				db.Create(&game.Game{Name: "test game", Owners: owners1})
-				db.Create(&game.Game{Name: "test game 2", Owners: owners2})
+				err = tx.Commit()
+				if err != nil {
+					t.Fatalf("Failed to commit transaction: %v", err)
+				}
 			},
 			expectedStatusCode: http.StatusOK,
 			expectedBody:       `{"games":[{"id":1,"name":"test game","owners":[{"id":1,"sub":"mock-sub"}]}]}`,
 			headers:            map[string]string{"Authorization": "permitted"},
-		},
-		// POST /games to create a new game
+		}, // POST /games to create a new game
 		{
 			name:     "POST create game",
 			method:   "POST",
 			endpoint: "/games",
 			body:     `{"name":"new game"}`,
-			setup: func(db *gorm.DB) {
+			setup: func(db *sql.DB) {
 				// No initial setup needed
 			},
 			expectedStatusCode: http.StatusCreated,
 			expectedBody:       `{"id":1,"name":"new game","owners":[{"id":1,"sub":"mock-sub"}]}`,
 			headers:            map[string]string{"Authorization": "permitted", "Content-Type": "application/json"},
-		},
-		// PUT /games/:id to update a game
+		}, // PUT /games/:id to update a game
 		{
-			name:   "PUT update game",
-			method: "PUT",
-			// Assuming the game ID is 1
+			name:     "PUT update game",
+			method:   "PUT", // Assuming the game ID is 1
 			endpoint: "/games/1",
 			body:     `{"name":"updated game"}`,
-			setup: func(db *gorm.DB) {
-				owners := []game.Owner{{Sub: "mock-sub"}}
-				db.Create(&game.Game{ID: 1, Name: "existing game", Owners: owners})
+			setup: func(db *sql.DB) {
+				tx, err := db.Begin()
+				if err != nil {
+					t.Fatalf("Failed to begin transaction: %v", err)
+				}
+				_, err = tx.Exec(`INSERT INTO owners (id,sub) VALUES (1, 'mock-sub')`)
+				if err != nil {
+					t.Fatalf("Failed to insert owners: %v", err)
+				}
+				_, err = tx.Exec(`INSERT INTO games (id, name) VALUES (1, 'game to update')`)
+				if err != nil {
+					t.Fatalf("Failed to insert games: %v", err)
+				}
+				_, err = tx.Exec(`INSERT INTO game_owners (game_id, owner_id) VALUES (1, 1)`)
+				if err != nil {
+					t.Fatalf("Failed to insert game_owners: %v", err)
+				}
+
+				err = tx.Commit()
+				if err != nil {
+					t.Fatalf("Failed to commit transaction: %v", err)
+				}
 			},
 			expectedStatusCode: http.StatusOK,
 			expectedBody:       `{"id":1,"name":"updated game","owners":[{"id":1,"sub":"mock-sub"}]}`,
 			headers:            map[string]string{"Authorization": "permitted", "Content-Type": "application/json"},
-		},
-		// DELETE /games/:id to delete a game
+		}, // DELETE /games/:id to delete a game
 		{
 			name:     "DELETE game",
 			method:   "DELETE",
 			endpoint: "/games/1",
-			setup: func(db *gorm.DB) {
-				owners := []game.Owner{{Sub: "mock-sub"}}
-				db.Create(&game.Game{ID: 1, Name: "game to delete", Owners: owners})
+			setup: func(db *sql.DB) {
+				tx, err := db.Begin()
+				if err != nil {
+					t.Fatalf("Failed to begin transaction: %v", err)
+				}
+				_, err = tx.Exec(`INSERT INTO owners (id,sub) VALUES (1, 'mock-sub')`)
+				if err != nil {
+					t.Fatalf("Failed to insert owners: %v", err)
+				}
+				_, err = tx.Exec(`INSERT INTO games (id, name) VALUES (1, 'game to delete')`)
+				if err != nil {
+					t.Fatalf("Failed to insert games: %v", err)
+				}
+				_, err = tx.Exec(`INSERT INTO game_owners (game_id, owner_id) VALUES (1, 1)`)
+				if err != nil {
+					t.Fatalf("Failed to insert game_owners: %v", err)
+				}
+
+				err = tx.Commit()
+				if err != nil {
+					t.Fatalf("Failed to commit transaction: %v", err)
+				}
 			},
 			expectedStatusCode: http.StatusNoContent,
 			expectedBody:       ``,
@@ -97,16 +145,15 @@ func TestGames(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			_, cleanup, _ := setupTestDatabase()
+			cleanup, _ := setupTestDatabase()
 			defer cleanup()
 
-			instance, server, teardown := setupTestServer()
+			server, teardown, db := setupTestServer()
 			defer teardown(server)
+			defer db.Close()
 
-			// Perform the setup specific to this test case
-			tc.setup(instance.DB)
+			tc.setup(db)
 
-			// Prepare the request body
 			var requestBody io.Reader
 			if tc.body != "" {
 				requestBody = bytes.NewBuffer([]byte(tc.body))
@@ -118,23 +165,19 @@ func TestGames(t *testing.T) {
 				t.Fatalf("Failed to create %s request: %v", tc.method, err)
 			}
 
-			// Set headers
 			for key, value := range tc.headers {
 				req.Header.Set(key, value)
 			}
 
-			// Execute the HTTP request
 			resp, err := http.DefaultClient.Do(req)
 			if err != nil {
 				t.Fatalf("Failed to perform %s request: %v", tc.method, err)
 			}
 			defer resp.Body.Close()
 
-			// Read the response body
 			bodyBytes, _ := io.ReadAll(resp.Body)
 			bodyString := string(bodyBytes)
 
-			// Assertions
 			assert.Equal(t, tc.expectedStatusCode, resp.StatusCode, "Expected status code %d", tc.expectedStatusCode)
 
 			if tc.expectedBody != "" {
