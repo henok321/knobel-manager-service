5583c59fa7ea9ac4d9a7cbd16c7334d28761b533
Hendrik Brinkmann <henok321@gmail.com>
2024-10-22 16:54:31 +0200

    chore: move hooks to script folder

--- scripts/install_hooks.sh
+++ scripts/install_hooks.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 # Define the source of the hook script
-HOOK_SRC="./hooks/pre-commit"
+HOOK_SRC="./scripts/hooks/pre-commit"
 
 # Define the destination in the .git/hooks directory
 HOOK_DEST=".git/hooks/pre-commit"
 
 # Check if the .git directory exists (i.e., the script is run inside a Git repo)
 if [ ! -d ".git" ]; then
-  echo "This is not a Git repository!"
+  echo "ERROR: This is not a Git repository!"
+  exit 1
+fi
+
+if [ ! -f "$HOOK_SRC" ]; then
+  echo "ERROR: The pre-commit hook script is missing!"
   exit 1
 fi
 
