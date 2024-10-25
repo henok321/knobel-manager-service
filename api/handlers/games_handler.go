0625406cc064d5bf8ee6918c692479a04d715f51
Hendrik Brinkmann <henok321@gmail.com>
2024-10-26 00:20:55 +0200

    refactor: global error handling

--- api/handlers/games_handler.go
+++ api/handlers/games_handler.go
@@ -1,11 +1,8 @@
 package handlers
 
 import (
-	"errors"
 	"net/http"
 	"strconv"
-
-	"github.com/henok321/knobel-manager-service/pkg/entity"
 
 	"github.com/henok321/knobel-manager-service/pkg/game"
 
@@ -32,8 +29,9 @@ func (h *gamesHandler) GetGames(c *gin.Context) {
 	sub := c.GetStringMap("user")["sub"].(string)
 
 	games, err := h.service.FindAllByOwner(sub)
+
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		_ = c.Error(err)
 		return
 	}
 
@@ -45,22 +43,14 @@ func (h *gamesHandler) GetGameByID(c *gin.Context) {
 	gameID, err := strconv.ParseUint(c.Param("gameID"), 10, 64)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid gameID"})
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid gameID"})
 		return
 	}
 
 	gameById, err := h.service.FindByID(uint(gameID), sub)
+
 	if err != nil {
-
-		if errors.Is(err, entity.ErrorGameNotFound) {
-			c.JSON(http.StatusNotFound, gin.H{"error": "game not found"})
-			return
-		} else if errors.Is(err, entity.ErrorNotOwner) {
-			c.JSON(http.StatusForbidden, gin.H{"error": "user is not the owner of the game"})
-			return
-		}
-
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		_ = c.Error(err)
 		return
 	}
 
@@ -69,26 +59,19 @@ func (h *gamesHandler) GetGameByID(c *gin.Context) {
 
 func (h *gamesHandler) CreateGame(c *gin.Context) {
 
-	contentType := c.GetHeader("Content-Type")
-
-	if contentType != "application/json" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid content type"})
-		return
-	}
-
 	sub := c.GetStringMap("user")["sub"].(string)
 
 	gameCreateRequest := game.GameRequest{}
 
 	if err := c.ShouldBindJSON(&gameCreateRequest); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		_ = c.Error(err)
 		return
 	}
 
 	createdGame, err := h.service.CreateGame(sub, &gameCreateRequest)
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		_ = c.Error(err)
 		return
 	}
 
@@ -101,31 +84,21 @@ func (h *gamesHandler) UpdateGame(c *gin.Context) {
 	gameID, err := strconv.ParseUint(c.Param("gameID"), 10, 64)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid gameID"})
+		_ = c.Error(err)
 		return
 	}
 
 	gameUpdateRequest := game.GameRequest{}
 
 	if err := c.ShouldBindJSON(&gameUpdateRequest); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		_ = c.Error(err)
 		return
 	}
 
 	updatedGame, err := h.service.UpdateGame(uint(gameID), sub, gameUpdateRequest)
 
 	if err != nil {
-
-		if errors.Is(err, entity.ErrorGameNotFound) {
-			c.JSON(http.StatusNotFound, gin.H{"error": "game not found"})
-			return
-		}
-		if errors.Is(err, entity.ErrorNotOwner) {
-			c.JSON(http.StatusForbidden, gin.H{"error": "user is not the owner of the game"})
-			return
-		}
-
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		_ = c.Error(err)
 		return
 	}
 
@@ -136,24 +109,13 @@ func (h *gamesHandler) DeleteGame(c *gin.Context) {
 	sub := c.GetStringMap("user")["sub"].(string)
 	gameID, err := strconv.ParseUint(c.Param("gameID"), 10, 64)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		_ = c.Error(err)
 		return
 	}
 
-	err = h.service.DeleteGame(uint(gameID), sub)
-
-	if err != nil {
-		if errors.Is(err, entity.ErrorGameNotFound) {
-			c.JSON(http.StatusNotFound, gin.H{"error": "game not found"})
-			return
-		}
-		if errors.Is(err, entity.ErrorNotOwner) {
-			c.JSON(http.StatusForbidden, gin.H{"error": "user is not the owner of the game"})
-			return
-		}
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	if err := h.service.DeleteGame(uint(gameID), sub); err != nil {
+		_ = c.Error(err)
 		return
 	}
-
 	c.Status(http.StatusNoContent)
 }
--- api/middleware/error_handler.go
+++ api/middleware/error_handler.go
@@ -1,16 +1,41 @@
+// api/middleware/error_handler.go
 package middleware
 
 import (
+	"errors"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
+
 	"github.com/gin-gonic/gin"
+	"github.com/henok321/knobel-manager-service/pkg/entity"
 )
 
 func ErrorHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
+		if c.Writer.Written() {
+			// Response has already been written by the handler
+			return
+		}
 		if len(c.Errors) > 0 {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": c.Errors})
+			err := c.Errors[0].Err
+			switch {
+			case errors.Is(err, entity.ErrorGameNotFound):
+				c.JSON(http.StatusNotFound, gin.H{"error": "game not found"})
+			case errors.Is(err, entity.ErrorTeamNotFound):
+				c.JSON(http.StatusNotFound, gin.H{"error": "team not found"})
+			case errors.Is(err, entity.ErrorNotOwner):
+				c.JSON(http.StatusForbidden, gin.H{"error": "user is not the owner of the game"})
+			default:
+				// Handle validation errors and other errors
+				var validationErr validator.ValidationErrors
+				if errors.As(err, &validationErr) {
+					c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+				} else {
+					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				}
+			}
 		}
 	}
 }
