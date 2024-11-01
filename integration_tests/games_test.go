9749ccc968a2c0d6c7c50f7fbb85b27712ee6e20
Hendrik Brinkmann <henok321@gmail.com>
2024-11-01 22:25:32 +0100

    test: refactor structure

--- integration_tests/games_test.go
+++ integration_tests/games_test.go
@@ -8,86 +8,62 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func gamesTestCases(t *testing.T) []testCase {
-	return []testCase{
-		{
-			name:               "GET games empty",
+func TestGames(t *testing.T) {
+	tests := map[string]testCase{
+		"GET games empty": {
 			method:             "GET",
 			endpoint:           "/games",
 			expectedStatusCode: http.StatusOK,
 			expectedBody:       `{"games":[]}`,
 			requestHeaders:     map[string]string{"Authorization": "sub-1"},
 		},
-		{
-			name:     "GET games",
+		"GET games": {
 			method:   "GET",
 			endpoint: "/games",
 			setup: func(db *sql.DB) {
-				err := executeSQLFile(t, db, "./test_data/games_setup.sql")
-				if err != nil {
-					t.Fatalf("Failed to execute SQL file: %v", err)
-				}
-			},
-			expectedStatusCode: http.StatusOK,
-			expectedBody:       readContentFromFile(t, "./test_data/games_setup.json"),
-			requestHeaders:     map[string]string{"Authorization": "sub-1"},
+				executeSQLFile(t, db, "./test_data/games_setup.sql")
+			}, expectedStatusCode: http.StatusOK,
+			expectedBody:   readContentFromFile(t, "./test_data/games_setup.json"),
+			requestHeaders: map[string]string{"Authorization": "sub-1"},
 		},
-		{
-			name:     "GET game by id ok",
+		"GET game by id ok": {
 			method:   "GET",
 			endpoint: "/games/1",
 			setup: func(db *sql.DB) {
-				err := executeSQLFile(t, db, "./test_data/games_setup.sql")
-				if err != nil {
-					t.Fatalf("Failed to execute SQL file: %v", err)
-				}
-			},
-			expectedStatusCode: http.StatusOK,
-			expectedBody:       readContentFromFile(t, "./test_data/games_setup_by_id.json"),
-			requestHeaders:     map[string]string{"Authorization": "sub-1"},
+				executeSQLFile(t, db, "./test_data/games_setup.sql")
+			}, expectedStatusCode: http.StatusOK,
+			expectedBody:   readContentFromFile(t, "./test_data/games_setup_by_id.json"),
+			requestHeaders: map[string]string{"Authorization": "sub-1"},
 		},
-		{
-			name:     "GET game by id not found",
+		"GET game by id not found": {
 			method:   "GET",
 			endpoint: "/games/2",
 			setup: func(db *sql.DB) {
-				err := executeSQLFile(t, db, "./test_data/games_setup.sql")
-				if err != nil {
-					t.Fatalf("Failed to execute SQL file: %v", err)
-				}
-			},
-			expectedStatusCode: http.StatusNotFound,
-			expectedBody:       `{"error":"game not found"}`,
-			requestHeaders:     map[string]string{"Authorization": "sub-1"},
-		}, {
-			name:     "GET game by id invalid gameID",
+				executeSQLFile(t, db, "./test_data/games_setup.sql")
+			}, expectedStatusCode: http.StatusNotFound,
+			expectedBody:   `{"error":"game not found"}`,
+			requestHeaders: map[string]string{"Authorization": "sub-1"},
+		},
+		"GET game by id invalid gameID": {
 			method:   "GET",
 			endpoint: "/games/invalid",
 			setup: func(db *sql.DB) {
-				err := executeSQLFile(t, db, "./test_data/games_setup.sql")
-				if err != nil {
-					t.Fatalf("Failed to execute SQL file: %v", err)
-				}
-			},
-			expectedStatusCode: http.StatusBadRequest,
-			expectedBody:       `{"error":"invalid gameID"}`,
-			requestHeaders:     map[string]string{"Authorization": "sub-1"},
-		}, {
-			name:     "GET game by id not owner",
+				executeSQLFile(t, db, "./test_data/games_setup.sql")
+			}, expectedStatusCode: http.StatusBadRequest,
+			expectedBody:   `{"error":"invalid gameID"}`,
+			requestHeaders: map[string]string{"Authorization": "sub-1"},
+		},
+		"GET game by id not owner": {
 			method:   "GET",
 			endpoint: "/games/1",
 			setup: func(db *sql.DB) {
-				err := executeSQLFile(t, db, "./test_data/games_setup.sql")
-				if err != nil {
-					t.Fatalf("Failed to execute SQL file: %v", err)
-				}
+				executeSQLFile(t, db, "./test_data/games_setup.sql")
 			},
 			expectedStatusCode: http.StatusForbidden,
 			expectedBody:       `{"error":"user is not the owner of the game"}`,
 			requestHeaders:     map[string]string{"Authorization": "sub-2"},
 		},
-		{
-			name:               "Create new game",
+		"Create new game": {
 			method:             "POST",
 			endpoint:           "/games",
 			expectedStatusCode: http.StatusCreated,
@@ -96,16 +72,14 @@ func gamesTestCases(t *testing.T) []testCase {
 			expectedBody:       `{"game":{"id":1,"name":"Game 1","teamSize":4,"tableSize":4,"numberOfRounds":2,"status":"setup","owners":[{"gameID":1,"ownerSub":"sub-1"}]}}`,
 			expectedHeaders:    map[string]string{"Location": "/games/1"},
 		},
-		{
-			name:               "Create new game invalid request",
+		"Create new game invalid request": {
 			method:             "POST",
 			endpoint:           "/games",
 			expectedStatusCode: http.StatusBadRequest,
 			requestBody:        `{}`,
 			requestHeaders:     map[string]string{"Authorization": "sub-1"},
 		},
-		{
-			name:               "Update an existing game",
+		"Update an existing game": {
 			method:             "PUT",
 			endpoint:           "/games/1",
 			requestBody:        `{"name":"Game 1 updated","numberOfRounds":3, "teamSize":4, "tableSize":4}`,
@@ -113,78 +87,84 @@ func gamesTestCases(t *testing.T) []testCase {
 			expectedStatusCode: http.StatusOK,
 			expectedBody:       `{"game":{"id":1,"name":"Game 1 updated","teamSize":4,"tableSize":4,"numberOfRounds":3,"status":"setup","owners":[{"gameID":1,"ownerSub":"sub-1"}]}}`,
 			setup: func(db *sql.DB) {
-				err := executeSQLFile(t, db, "./test_data/games_setup.sql")
-				if err != nil {
-					t.Fatalf("Failed to execute SQL file: %v", err)
-				}
+				executeSQLFile(t, db, "./test_data/games_setup.sql")
 			},
 		},
-		{
-			name:               "Update an existing game invalid request",
+		"Update an existing game invalid request": {
 			method:             "PUT",
 			endpoint:           "/games/1",
 			requestBody:        `{"name":"Game 1 updated","numberOfRounds":3}`,
 			requestHeaders:     map[string]string{"Authorization": "sub-1"},
 			expectedStatusCode: http.StatusBadRequest,
 		},
-		{
-			name:               "Update an existing game not found",
+		"Update an existing game not found": {
 			method:             "PUT",
 			endpoint:           "/games/1",
 			requestBody:        `{"name":"Game 1 updated","numberOfRounds":3, "teamSize":4, "tableSize":4}`,
 			requestHeaders:     map[string]string{"Authorization": "sub-1"},
 			expectedStatusCode: http.StatusNotFound,
 		},
-		{
-			name:               "Update an existing game not owner",
+		"Update an existing game not owner": {
 			method:             "PUT",
 			endpoint:           "/games/1",
 			requestBody:        `{"name":"Game 1 updated","numberOfRounds":3, "teamSize":4, "tableSize":4}`,
 			requestHeaders:     map[string]string{"Authorization": "sub-2"},
 			expectedStatusCode: http.StatusForbidden,
 			setup: func(db *sql.DB) {
-				err := executeSQLFile(t, db, "./test_data/games_setup.sql")
-				if err != nil {
-					t.Fatalf("Failed to execute SQL file: %v", err)
-				}
+				executeSQLFile(t, db, "./test_data/games_setup.sql")
+
 			},
 		},
-		{
-			name:               "Delete an existing game",
+		"Delete an existing game": {
 			method:             "DELETE",
 			endpoint:           "/games/1",
 			requestHeaders:     map[string]string{"Authorization": "sub-1"},
 			expectedStatusCode: http.StatusNoContent,
 			setup: func(db *sql.DB) {
-				err := executeSQLFile(t, db, "./test_data/games_setup.sql")
-				if err != nil {
-					t.Fatalf("Failed to execute SQL file: %v", err)
-				}
+				executeSQLFile(t, db, "./test_data/games_setup.sql")
+
 			},
 		},
-		{
-			name:               "Delete an existing game not found",
+		"Delete an existing game not found": {
 			method:             "DELETE",
 			endpoint:           "/games/1",
 			requestHeaders:     map[string]string{"Authorization": "sub-1"},
 			expectedStatusCode: http.StatusNotFound,
 		},
-		{
-			name:               "Delete an existing game not owner",
+		"Delete an existing game not owner": {
 			method:             "DELETE",
 			endpoint:           "/games/1",
 			requestHeaders:     map[string]string{"Authorization": "sub-2"},
 			expectedStatusCode: http.StatusForbidden,
 			setup: func(db *sql.DB) {
-				err := executeSQLFile(t, db, "./test_data/games_setup.sql")
-				if err != nil {
-					t.Fatalf("Failed to execute SQL file: %v", err)
-				}
+				executeSQLFile(t, db, "./test_data/games_setup.sql")
 			},
 		},
 	}
-}
 
-func TestGames(t *testing.T) {
-	RunTestGroup(t, gamesTestCases)
+	dbConn, teardownDatabase := setupTestDatabase(t)
+	defer teardownDatabase()
+
+	db, err := sql.Open("postgres", dbConn)
+
+	if err != nil {
+		t.Fatalf("Failed to open database connection: %v", err)
+	}
+
+	defer db.Close()
+
+	runGooseUp(t, db)
+
+	server, teardown := setupTestServer()
+	defer teardown(server)
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			if tc.setup != nil {
+				tc.setup(db)
+			}
+			defer executeSQLFile(t, db, "./test_data/cleanup.sql")
+			newTestRequest(t, tc, server)
+		})
+	}
 }
--- integration_tests/integration_test.go
+++ integration_tests/integration_test.go
@@ -1,8 +1,10 @@
 package integration_tests
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
+	"io"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -12,14 +14,58 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/pressly/goose/v3"
-
 	"github.com/henok321/knobel-manager-service/internal/app"
+	"github.com/pressly/goose/v3"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/stretchr/testify/assert"
 )
+
+type testCase struct {
+	method             string
+	endpoint           string
+	requestBody        string
+	requestHeaders     map[string]string
+	setup              func(db *sql.DB)
+	expectedStatusCode int
+	expectedBody       string
+	expectedHeaders    map[string]string
+}
+
+func newTestRequest(t *testing.T, tc testCase, server *httptest.Server) {
+	var requestBody io.Reader
+	if tc.requestBody != "" {
+		requestBody = bytes.NewBuffer([]byte(tc.requestBody))
+	}
+
+	// Create the HTTP request
+	req, err := http.NewRequest(tc.method, server.URL+tc.endpoint, requestBody)
+	if err != nil {
+		t.Fatalf("Failed to create %s request: %v", tc.method, err)
+	}
+
+	for key, value := range tc.requestHeaders {
+		req.Header.Set(key, value)
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("Failed to perform %s request: %v", tc.method, err)
+	}
+	defer resp.Body.Close()
+
+	responseBodyBytes, _ := io.ReadAll(resp.Body)
+	responseBodyString := string(responseBodyBytes)
+
+	assert.Equal(t, tc.expectedStatusCode, resp.StatusCode, "Expected status code %d", tc.expectedStatusCode)
+
+	if tc.expectedBody != "" {
+		assert.JSONEq(t, tc.expectedBody, responseBodyString)
+	}
+}
 
 func readContentFromFile(t *testing.T, filepath string) string {
 	content, err := os.ReadFile(filepath)
@@ -30,7 +76,7 @@ func readContentFromFile(t *testing.T, filepath string) string {
 	return string(content)
 }
 
-func executeSQLFile(t *testing.T, db *sql.DB, filepath string) error {
+func executeSQLFile(t *testing.T, db *sql.DB, filepath string) {
 	content, err := os.ReadFile(filepath)
 	if err != nil {
 		t.Fatalf("failed to read SQL file: %v", err)
@@ -39,18 +85,16 @@ func executeSQLFile(t *testing.T, db *sql.DB, filepath string) error {
 	if err != nil {
 		t.Fatalf("failed to execute SQL file: %v", err)
 	}
-	return nil
 }
 
-func runGooseUp(t *testing.T, db *sql.DB) error {
+func runGooseUp(t *testing.T, db *sql.DB) {
 	migrationsDir := filepath.Join("..", "db_migration")
 	if err := goose.SetDialect("postgres"); err != nil {
-		t.Fatalf("failed to set dialect: %v", err)
+		t.Fatalf("goose failed to set dialect: %v", err)
 	}
 	if err := goose.Up(db, migrationsDir); err != nil {
-		t.Fatalf("failed to run migrations: %v", err)
+		t.Fatalf("goose failed to run migrations: %v", err)
 	}
-	return nil
 }
 
 func mockAuthMiddleware() gin.HandlerFunc {
@@ -84,7 +128,7 @@ func setupTestServer() (*httptest.Server, func(*httptest.Server)) {
 	return server, teardown
 }
 
-func setupTestDatabase(t *testing.T) (string, func(), error) {
+func setupTestDatabase(t *testing.T) (string, func()) {
 	ctx := context.Background()
 	pgContainer, err := postgres.Run(ctx, "docker.io/postgres:16-alpine", postgres.WithDatabase("knobel-manager-service"), postgres.WithUsername("test"), postgres.WithPassword("secret"), testcontainers.WithWaitStrategy(wait.ForLog("database system is ready to accept connections").
 		WithOccurrence(2).WithStartupTimeout(5*time.Second)))
@@ -107,5 +151,5 @@ func setupTestDatabase(t *testing.T) (string, func(), error) {
 		}
 	}
 
-	return connStr, teardown, nil
+	return connStr, teardown
 }
