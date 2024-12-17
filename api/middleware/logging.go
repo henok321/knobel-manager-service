2c3fb86d8b29990381210a1821eadb32a7db4a33
Hendrik Brinkmann <henok321@gmail.com>
2024-12-17 10:51:05 +0100

    refactor: simplify logging middleware

--- api/middleware/logging.go
+++ api/middleware/logging.go
@@ -1,25 +1,14 @@
 package middleware
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 )
 
 func RequestLogging(logLevel slog.Level, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		switch logLevel {
-		case slog.LevelDebug:
-			slog.Debug("Incoming request", "method", request.Method, "path", request.URL.Path)
-		case slog.LevelInfo:
-			slog.Info("Incoming request", "method", request.Method, "path", request.URL.Path)
-		case slog.LevelWarn:
-			slog.Warn("Incoming request", "method", request.Method, "path", request.URL.Path)
-		case slog.LevelError:
-			slog.Error("Incmming request", "method", request.Method, "path", request.URL.Path)
-		default:
-			slog.Info("Incoming request", "method", request.Method, "path", request.URL.Path)
-		}
-
+		slog.Log(context.Background(), logLevel, "Incoming request", "method", request.Method, "path", request.URL.Path)
 		next.ServeHTTP(writer, request)
 	})
 }
