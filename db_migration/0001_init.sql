7292faff979623f1629a066d5bd5a8c3af8a8b2d
Hendrik Brinkmann <henok321@gmail.com>
2025-05-05 17:30:10 +0200

    chore: sqlfluff linter

--- db_migration/0001_init.sql
+++ db_migration/0001_init.sql
@@ -6,15 +6,15 @@ CREATE TYPE game_status AS ENUM ('setup', 'in_progress', 'completed');
 -- Table: games
 CREATE TABLE games
 (
-    id               SERIAL PRIMARY KEY,
-    name             VARCHAR(255)             NOT NULL,
-    team_size        INTEGER                  NOT NULL,
-    table_size       INTEGER                  NOT NULL,
-    number_of_rounds INTEGER                  NOT NULL,
-    status           game_status              NOT NULL,
-    created_at       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    updated_at       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    deleted_at       TIMESTAMP WITH TIME ZONE,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    team_size INTEGER NOT NULL,
+    table_size INTEGER NOT NULL,
+    number_of_rounds INTEGER NOT NULL,
+    status GAME_STATUS NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE,
     UNIQUE (id, deleted_at)
 );
 
@@ -24,7 +24,7 @@ CREATE INDEX idx_games_deleted_at ON games (deleted_at);
 -- Table: game_owners
 CREATE TABLE game_owners
 (
-    game_id   INTEGER      NOT NULL REFERENCES games (id) ON DELETE CASCADE,
+    game_id INTEGER NOT NULL REFERENCES games (id) ON DELETE CASCADE,
     owner_sub VARCHAR(255) NOT NULL,
     PRIMARY KEY (game_id, owner_sub)
 );
@@ -35,10 +35,12 @@ CREATE INDEX idx_game_owners_owner_sub ON game_owners (owner_sub);
 -- Table: team
 CREATE TABLE teams
 (
-    id      SERIAL PRIMARY KEY,
-    name    VARCHAR(255) NOT NULL,
-    game_id INTEGER      NOT NULL REFERENCES games (id) ON DELETE CASCADE,
-    CONSTRAINT fk_team_game FOREIGN KEY (game_id) REFERENCES games (id) ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    game_id INTEGER NOT NULL REFERENCES games (id) ON DELETE CASCADE,
+    CONSTRAINT fk_team_game FOREIGN KEY (game_id) REFERENCES games (
+        id
+    ) ON DELETE CASCADE
 );
 
 -- Index on game_id
@@ -47,10 +49,12 @@ CREATE INDEX idx_teams_game_id ON teams (game_id);
 -- Table: players
 CREATE TABLE players
 (
-    id      SERIAL PRIMARY KEY,
-    name    VARCHAR(255) NOT NULL,
-    team_id INTEGER      NOT NULL REFERENCES teams (id) ON DELETE CASCADE,
-    CONSTRAINT fk_player_team FOREIGN KEY (team_id) REFERENCES teams (id) ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    team_id INTEGER NOT NULL REFERENCES teams (id) ON DELETE CASCADE,
+    CONSTRAINT fk_player_team FOREIGN KEY (team_id) REFERENCES teams (
+        id
+    ) ON DELETE CASCADE
 );
 
 -- Index on team_id
@@ -59,10 +63,10 @@ CREATE INDEX idx_players_team_id ON players (team_id);
 -- Table: rounds
 CREATE TABLE rounds
 (
-    id           SERIAL PRIMARY KEY,
-    round_number INTEGER     NOT NULL,
-    game_id      INTEGER     NOT NULL REFERENCES games (id) ON DELETE CASCADE,
-    status       VARCHAR(50) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    round_number INTEGER NOT NULL,
+    game_id INTEGER NOT NULL REFERENCES games (id) ON DELETE CASCADE,
+    status VARCHAR(50) NOT NULL,
     CONSTRAINT unique_game_round UNIQUE (game_id, round_number)
 );
 
@@ -72,9 +76,9 @@ CREATE INDEX idx_rounds_game_id ON rounds (game_id);
 -- Table: game_tables
 CREATE TABLE game_tables
 (
-    id           SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     table_number INTEGER NOT NULL,
-    round_id     INTEGER NOT NULL REFERENCES rounds (id) ON DELETE CASCADE,
+    round_id INTEGER NOT NULL REFERENCES rounds (id) ON DELETE CASCADE,
     CONSTRAINT unique_round_table UNIQUE (round_id, table_number)
 );
 
@@ -84,7 +88,9 @@ CREATE INDEX idx_game_tables_round_id ON game_tables (round_id);
 -- Table: table_players
 CREATE TABLE table_players
 (
-    game_table_id  INTEGER NOT NULL REFERENCES game_tables (id) ON DELETE CASCADE,
+    game_table_id INTEGER NOT NULL REFERENCES game_tables (
+        id
+    ) ON DELETE CASCADE,
     player_id INTEGER NOT NULL REFERENCES players (id) ON DELETE CASCADE,
     PRIMARY KEY (game_table_id, player_id)
 );
@@ -96,10 +102,10 @@ CREATE INDEX idx_table_players_player_id ON table_players (player_id);
 -- Table: scores
 CREATE TABLE scores
 (
-    id        SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     player_id INTEGER NOT NULL REFERENCES players (id) ON DELETE CASCADE,
-    table_id  INTEGER NOT NULL REFERENCES game_tables (id) ON DELETE CASCADE,
-    score     INTEGER NOT NULL,
+    table_id INTEGER NOT NULL REFERENCES game_tables (id) ON DELETE CASCADE,
+    score INTEGER NOT NULL,
     CONSTRAINT unique_player_table UNIQUE (player_id, table_id)
 );
 
--- db_migration/0002_active_game.sql
+++ db_migration/0002_active_game.sql
@@ -4,5 +4,5 @@
 CREATE TABLE active_games
 (
     owner_sub VARCHAR(255) NOT NULL PRIMARY KEY,
-    game_id   INTEGER      NOT NULL REFERENCES games (id) ON DELETE CASCADE
+    game_id INTEGER NOT NULL REFERENCES games (id) ON DELETE CASCADE
 );
