8083ec65afeea0fd65baf6aa6375cca4d7857298
Hendrik Brinkmann <henok321@gmail.com>
2025-05-05 13:25:16 +0200

    chore: pre-commot hook to format shell scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,6 @@ echo "Install git hooks..."
 
 pre-commit install --hook-type pre-commit --hook-type pre-push
 
-
 echo "Setup database..."
 docker-compose up -d
 
@@ -14,11 +13,16 @@ export GOOSE_DRIVER=postgres
 export GOOSE_MIGRATION_DIR="./db_migration"
 export GOOSE_DBSTRING="postgres://postgres:secret@localhost:5432/postgres?sslmode=disable"
 
+until docker exec knobel-manager-service-db-1 pg_isready -h localhost -p 5432; do
+	echo "Waiting for PostgreSQL to start..."
+	sleep 2
+done
+
 goose validate
 goose up
 
 echo "Init .env..."
 
-echo "ENVIRONMENT=local" > .env
-echo "FIREBASE_SECRET=$(jq -c . ./firebaseServiceAccount.json | base64)" >> .env
-echo "DATABASE_URL=$DATABASE_URL" >> .env
+echo "ENVIRONMENT=local" >.env
+echo "FIREBASE_SECRET=$(jq -c . ./firebaseServiceAccount.json | base64)" >>.env
+echo "DATABASE_URL=$DATABASE_URL" >>.env
