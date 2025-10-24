60c29c6bc3162a8892ab8094ca78b429856b0905
Hendrik Brinkmann <henok321@gmail.com>
2025-10-24 12:51:40 +0200

    chore: security header

--- api/middleware/security_headers.go
+++ api/middleware/security_headers.go
@@ -25,20 +25,15 @@ func SecurityHeaders(contentSecurityPolicy string, next http.Handler) http.Handl
 		}
 
 		w.Header().Set("X-Content-Type-Options", "nosniff")
-
 		w.Header().Set("X-Frame-Options", "DENY")
-
-		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("Content-Security-Policy", contentSecurityPolicy)
+		w.Header().Set("Cors-Origin-Resource-Policy", "same-origin")
+		w.Header().Set("Referrer-Policy", "no-referrer")
+		w.Header().Set("Cache-Control", "no-cache")
 
 		if r.TLS != nil {
 			w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
 		}
-
-		w.Header().Set("Content-Security-Policy", contentSecurityPolicy)
-
-		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
-
-		w.Header().Set("Permissions-Policy", "geolocation=(), microphone=(), camera=()")
 
 		next.ServeHTTP(w, r)
 	})
