b0a0e377d0da7f4797efe0bfc9a26c409425e2c3
Hendrik Brinkmann <henok321@gmail.com>
2026-02-06 13:08:33 +0100

    chore: health test database down

--- integrationtests/health_test.go
+++ integrationtests/health_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 func TestHealthCheck(t *testing.T) {
-	t.Run("liveness check", func(t *testing.T) {
+	t.Run("liveness check pass", func(t *testing.T) {
 		dbConn, teardownDatabase := setupTestDatabase(t)
 		defer teardownDatabase()
 
@@ -42,7 +42,7 @@ func TestHealthCheck(t *testing.T) {
 		assert.Equal(t, "pass", response.Status)
 	})
 
-	t.Run("readiness check - healthy", func(t *testing.T) {
+	t.Run("readiness check pass", func(t *testing.T) {
 		dbConn, teardownDatabase := setupTestDatabase(t)
 		defer teardownDatabase()
 
@@ -77,6 +77,49 @@ func TestHealthCheck(t *testing.T) {
 		dbCheck, exists := checks["database"]
 		require.True(t, exists, "Database check should be present")
 		assert.Equal(t, health.HealthCheckDetailedResponseChecksStatus("pass"), dbCheck.Status)
+
+		firebaseCheck, exists := checks["firebase"]
+		require.True(t, exists, "Firebase check should be present")
+		assert.Equal(t, health.HealthCheckDetailedResponseChecksStatus("pass"), firebaseCheck.Status)
+	})
+
+	t.Run("readiness check database not available fail", func(t *testing.T) {
+		dbConn, teardownDatabase := setupTestDatabase(t)
+
+		db, err := sql.Open("postgres", dbConn)
+		if err != nil {
+			t.Fatalf("Failed to open database connection: %v", err)
+		}
+
+		defer db.Close()
+
+		runGooseUp(t, db)
+
+		server, teardown := setupTestServer(t)
+		defer teardown(server)
+
+		// Stop database to simulate database not available
+		teardownDatabase()
+
+		resp, err := http.Get(server.URL + "/health/ready")
+		if err != nil {
+			t.Fatalf("Failed to perform GET request: %v", err)
+		}
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusServiceUnavailable, resp.StatusCode, "Expected status code 503")
+
+		var response health.HealthCheckDetailedResponse
+		err = json.NewDecoder(resp.Body).Decode(&response)
+		require.NoError(t, err)
+		assert.Equal(t, health.HealthCheckDetailedResponseStatus("fail"), response.Status)
+
+		require.NotNil(t, response.Checks)
+		checks := *response.Checks
+
+		dbCheck, exists := checks["database"]
+		require.True(t, exists, "Database check should be present")
+		assert.Equal(t, health.HealthCheckDetailedResponseChecksStatus("fail"), dbCheck.Status)
 
 		firebaseCheck, exists := checks["firebase"]
 		require.True(t, exists, "Firebase check should be present")
